04703d44d2940f56925f0ac00a104f20d80ec513
Steve Baletsa <baletsa@gmail.com>
2018-01-08 01:13:21 -0500

    update sort to first sort by level then by name

--- app/filterStore.js
+++ app/filterStore.js
@@ -2,15 +2,54 @@ import { autorun, computed, toJS, action, observable } from 'mobx'
 
 import data from './data/spellData.json';
 
+const DefaultSort = (a, b) => {
+  const aLevel = a.level.toLowerCase(),
+        bLevel = b.level.toLowerCase(),
+        aName = a.name.toLowerCase(),
+        bName = b.name.toLowerCase()
+
+  if (aLevel.match(/\d+/) !== null && bLevel.match(/\d+/) !== null) {
+    if (aLevel.match(/\d+/)[0] === bLevel.match(/\d+/)[0]) {
+      return (aName < bName) ? -1 : ((aName > bName) ? 1 : 0)
+    } else if (aLevel.match(/\d+/)[0] > bLevel.match(/\d+/)[0]) {
+      return 1
+    } else if (aLevel.match(/\d+/)[0] < bLevel.match(/\d+/)[0]) {
+      return -1
+    } 
+  } else if (aLevel === 'cantrip' && bLevel === 'cantrip') {
+    return (aName < bName) ? -1 : ((aName > bName) ? 1 : 0)
+  } else if (aLevel === 'cantrip') {
+    return -1
+  } else if (bLevel === 'cantrip') {
+    return 1
+  } 
+}
 
 const SortByName = (a, b) => {
   const aName = a.name.toLowerCase(),
-    bName = b.name.toLowerCase();
-  
+        bName = b.name.toLowerCase()
   return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
 }
 
-let spellList = data.spells.sort(SortByName)
+const SortByLevel = (a, b) => {
+  const aLevel = a.level.toLowerCase(),
+        bLevel = b.level.toLowerCase()
+
+  if (aLevel.match(/\d+/) !== null && bLevel.match(/\d+/) !== null) {
+    if (aLevel.match(/\d+/)[0] > bLevel.match(/\d+/)[0]) {
+      return 1
+    } else if (aLevel.match(/\d+/)[0] < bLevel.match(/\d+/)[0]) {
+      return -1
+    } 
+  } else if (aLevel === 'cantrip') {
+    return -1
+  } else if (bLevel === 'cantrip') {
+    return 1
+  } 
+
+}
+
+let spellList = data.spells.sort(DefaultSort)
 
 class FilterStore {
   @observable spells = spellList
@@ -20,32 +59,26 @@ class FilterStore {
 
   @observable toggledButtons = []
   
-  @observable filters = {
-    'class': [],
-    'level': [],
-    'school': []
-  }
+  @observable classFilter = []
+  @observable levelFilter = []
+  @observable schoolFilter = []
+  @observable componentsFilter = []
+  @observable concentrationFilter = []
+  @observable ritualFilter = []
   
   @computed get filteredSpells() {
-    let results = this.spells.filter(sp =>
-      Object.keys(this.filters).every(key => 
-        this.filters[key].some(f => 
-          RegExp(f).test(sp[key]) 
-        )
-      )
-    )
+    let results = this.spells
     return results
   }
 
   @action
   toggleFilter(key, value) {
-
     if(this.filters[key].indexOf(value) !== -1) {
       this.filters[key].splice(this.filters[key].indexOf(value), 1)
     } else {
       this.filters[key].push(value)
     }
-    
+
     //console.log(key)
     //console.log(this.filters[key])
   }
@@ -79,7 +112,7 @@ class FilterStore {
 
   @action 
   clearFilter() {
-    console.log('clear filter')
+    this.toggledButtons = []
     this.filters = {} 
   }
 }
