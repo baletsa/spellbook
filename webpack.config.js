fe84060012c5a517c6076df2ae3ec8fde564da2e
Steve Baletsa <baletsa@gmail.com>
2018-01-09 21:46:13 -0500

    fixing css loading so that flash of un-styled content no longer happens

--- webpack.config.js
+++ webpack.config.js
@@ -27,19 +27,9 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [{
-          loader: "style-loader"
-        }, {
-          loader: "css-loader",
-          options: {
-            sourceMap: true
-          }
-        }, {
-          loader: "sass-loader",
-          options: {
-            sourceMap: true
-          }
-        }]
+        loader: ExtractTextPlugin.extract(
+          ['css-loader', 'sass-loader']
+        )
       },
       {
         test: /\.svg$/,
